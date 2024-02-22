7265aae4540b1db389ff0aea3ec5da22d85c82aa
natthinan27 <119989591+natthinan27@users.noreply.github.com>
2024-02-22 10:22:08 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -63,7 +63,7 @@ s8 = st.number_input("# Age : อายุของผู้ป่วย")
 
 # Adjusting font size and making it bold
 st.markdown("<style>h1{font-size: 18px !important;}</style>", unsafe_allow_html=True)
-st.markdown("<style>label{font-size: 16px !important; font-weight: bold;}</style>", unsafe_allow_html=True)
+st.markdown("<style>label{font-size: 16px !important; font-weight: bold;align-items: center; }</style>", unsafe_allow_html=True)
 
 if st.button("ทำนายผล"):
 
